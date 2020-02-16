422ccd6348dcc16ac2c2b22ab021eed656dcc8a9
Alexsandr Vobornov <vobos2@gmail.com>
2020-02-15 22:08:39 -0500

    Added Camera free-look script and movement based on camera direction instead of location relative to plane. Renamed asset folders

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public float hSpeed, vSpeed;
+    private float lookSpeed, mouseX, mouseY, moveSpeed;
+    private Rigidbody rb;
+    float moveForward, moveHorizontal, moveVertical;
+
+    void Start()
+    {
+        moveVertical = moveHorizontal = moveForward = 0.0f;
+
+        RaycastHit hit;
+        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            // Do something with the object that was hit by the raycast.
+        }
+        lookSpeed = 3.0f;
+        moveSpeed = 5.0f;
+        mouseX = mouseY = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+
+    }
+    private void movement()
+    {
+        mouseX += lookSpeed * Input.GetAxis("Mouse X");
+        mouseY -= lookSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0.0f);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            // Only if we are above ground
+            if (transform.position.y > 0.0f)
+            {
+                transform.position -= Vector3.up * Time.deltaTime * moveSpeed;
+            }
+
+        }
+
+
+        Vector3 input = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        transform.position += input * Time.deltaTime * moveSpeed;
+
+
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player, agentPrefab;
+    public CameraController camController;
+
+    public int agentNum;
+
+    void Start()
+    {
+        if (agentNum < 5)
+        {
+            agentNum = 5;
+        }
+        for (int i = 0; i < agentNum; i++)
+        {
+            //spawn agents
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
